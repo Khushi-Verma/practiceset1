3ffe8ddc3fba020ba308a2c95e8c91c29b77eddd
kvcs <44372851+Khushi-Verma@users.noreply.github.com>
2019-08-22 15:16:15 +0530

    for getting output

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
-        String userName="";
-        String name =scanner.nextLine();
-        System.out.println("Welcome,"+ name);
+        String userName =scanner.nextLine().trim();
+        System.out.println("Welcome,"+ userName+"!");
         scanner.close();
     }
 }
