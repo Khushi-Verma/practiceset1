bdfbbf26281c54b2b3ce0003524d4a32f2129d1a
kvcs <44372851+Khushi-Verma@users.noreply.github.com>
2019-08-24 15:53:49 +0530

    question 2

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        String enterName1;
+        String enterName2;
+        Scanner t=new Scanner(System.in);
+        Scanner b=new Scanner(System.in);
+        enterName1=t.nextLine();
+        enterName2=b.nextLine();
+        System.out.println(enterName1 + " University, "+ enterName2);
+    }
+}
